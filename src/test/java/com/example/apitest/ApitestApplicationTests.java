37ddde5852612765fa883213a73f60e241359516
NNSSPPP <168963651+NNSSPPP@users.noreply.github.com>
2025-07-24 09:46:46 +0700

    update

--- src/test/java/com/example/apitest/ApitestApplicationTests.java
+++ src/test/java/com/example/apitest/ApitestApplicationTests.java
@@ -3,11 +3,12 @@ package com.example.apitest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ApitestApplication.class)
 class ApitestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		//part test
 	}
 
 }
